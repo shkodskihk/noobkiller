3809dfad8429cb1bc11f6495ed4ad74184f912d3
Noiwex <noiwex1911@gmail.com>
2013-07-08 00:35:26 +0400

    Warning on long execution time

--- lua/hooks.lua
+++ lua/hooks.lua
@@ -14,7 +14,14 @@ end
 function hook.Call(hk, ...)
 	if hooks[hk] then
 		for k, fn in pairs(hooks[hk]) do
+			local t = os.clock()
+			
 			local ret = {fn(...)}
+
+			if (os.clock() - t) > .2 then
+				print("Warning! Hook", hk, k, " has been running for " .. (os.clock() - t) .. " seconds")
+			end
+
 			if #ret>0 then
 				return unpack(ret)
 			end

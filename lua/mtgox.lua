6d8691089edb36e68cdf4bb13cdeaa645e8b1370
Noiwex <noiwex1911@gmail.com>
2013-07-08 13:02:14 +0400

    mtgox bot?

---
+++ lua/mtgox.lua
@@ -0,0 +1,39 @@
+module("mtgox", package.seeall)
+
+local json=require'json'
+
+function Price(cb)
+	luasocket.Get('http://data.mtgox.com/api/1/BTCUSD/ticker_fast', function(tab)
+		local str = tab.content:gsub("^(.-){", "{"):gsub("}[%c%d]-$", "}")
+		local ret = json.decode(str)
+		cb(tonumber(ret["return"].last.value))
+	end)
+end
+
+function Update(cb)
+	Price(function(val)
+		LastPrice = CurPrice or val
+		CurPrice = val
+
+		if cb then
+			cb()
+		end
+	end)
+end
+
+function Shout()
+	Update(function()
+		local delta = CurPrice - LastPrice
+		Say("[mtgox] Current BTC Price: $" .. CurPrice .. " ( delta: $" .. delta .. " )")
+	end)
+end
+
+Update()
+
+timer.Create("mtgox", 600, 0, Shout)
+
+hook.Add("PersonSay", "mtgox", function(pl, line)
+	if line:find("^!btc",1) or line:find("^!bitcoin",1) or line:find("^!mtgox",1) then
+		Shout()
+	end
+end)

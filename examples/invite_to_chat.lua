085ee9a22fcfc43cfa198e1a14d43721ebd9c5de
Noiwex <noiwex1911@gmail.com>
2013-07-11 15:37:11 +0400

    Example file: Inviting in chat conference by commmand

---
+++ examples/invite_to_chat.lua
@@ -0,0 +1,21 @@
+local Blob = "123456789abcdef"
+
+local function InviteToBlob(usr, greet)
+	local chat = skype:FindChatUsingBlob(Blob)
+	local group = chat.Members
+	group:RemoveAll()
+	group:Add(usr)
+	skype:FindChatUsingBlob(Blob):AddMembers(group)
+
+	if greet then
+		timer.Simple(3, function()
+			chat:SendMessage("Greetings, " .. (usr.FullName ~= "" and usr.FullName or usr.Handle) .. "!")
+		end)
+	end
+end
+
+hook.Add("PersonSay", "Fabulous", function(pl, line)
+	if line:find("^!chat") then
+		InviteToBlob(pl, true)
+	end
+end)

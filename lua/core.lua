0d444ef150d390237a0bbd1e9302c4b26b08af26
Noiwex <noiwex1911@gmail.com>
2013-07-20 00:13:10 +0400

    Moved events into separate file

--- lua/core.lua
+++ lua/core.lua
@@ -12,85 +12,12 @@ local default_chat="TxjzXtCTc5yqq0I6O-8dBa2n7rDqsbTYf3ffCwduQ-0kFj5bn5UleK1pzqK0
 
 skype = luacom.CreateObject("Skype4COM.Skype", "Skype_")
 
-TAttachmentStatus = {
-	[-1] = "AttachUnknown",
-	[0] = "AttachSuccess",
-	[1] = "AttachPendingAuthorization",
-	[2] = "AttachRefused",
-	[3] = "AttachNotAvailable",
-	[4] = "AttachAvailable"	
-}
-
-TConnectionStatus = {
-	[-1] = "conUnknown",
-	[0] = "conOffline",
-	[1] = "conConnecting",
-	[2] = "conPausing",
-	[3] = "conOnline"
-}
-
-events = {}
-
-function events:AttachmentStatus(b)
-	print(TAttachmentStatus[b])
-end
-
-function events:ConnectionStatus(b)
-	print(TConnectionStatus[b])
-end
-
-function events:Command(b)
-	--print("CMD", b.Command) 
-end
-
-function events:Reply(b)
-	--print("REPLY", b.Reply)
-end
-
-function events:GroupUsers()
-end
-
-function events:ContactsFocused()
-end
-
-function events:MessageStatus(msg, status)
-	if (status == 1 or status == 2) and msg.Type == 4 then
-		hook.Call("PersonSay", msg.Sender, msg.Body, msg)
-	end
-end
-
-function events:UserAuthorizationRequestReceived(usr)
-	print(usr.handle .. " requests auth")
-	print("Text: ", usr.ReceivedAuthRequest)
-	hook.Call("AuthRequest", usr, usr.ReceivedAuthRequest)
-end
-
-function events:UserMood(usr, str)
-	print('[mood] ' .. usr.handle, str)
-end
-
-local OnlineStatus = {
-	[-1] = "crossed galaxy through matter and time",
-	[0] = "goes offline",
-	"goes online",
-	"is away",
-	"is not available",
-	"goes into DnD",
-	"SkypeOut",
-	"SkypeMe",
-}
-
-function events:OnlineStatus(usr, status)
-	print('[stat] ' .. usr.handle .. ' ' .. OnlineStatus(status))
-end
-
-setmetatable(events, {__index = function(_, key) print("Unhandled event: " .. key) end})
+require"events"
 
 if not skype.Client.IsRunning then
 	skype.Client:Start()
 end
 
-luacom.Connect(skype, events)
 skype:Attach(nil, false)
 
 math.randomseed(os.time()); math.random(); math.random()

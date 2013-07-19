f63b8c3fe84c13ef60c333d14ad399e40be319f4
Noiwex <noiwex1911@gmail.com>
2013-07-19 12:53:19 +0400

    No need in hacky refreshing stuff

--- lua/addons/hush.lua
+++ lua/addons/hush.lua
@@ -1,6 +1,5 @@
 hook.Add("PersonSay", "HushHal", function(pl, body, msg)
 	if pl.Handle == "hal1320" and msg.IsEditable and (body:find("^Twitter: ") or body:find("^Pastebin:")) then
 		msg.Body = "Access denied"
-		refresh()
 	end
 end)

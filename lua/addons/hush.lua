fd2c6c7b57be01e3147243682a8879646c104601
Noiwex <noiwex1911@gmail.com>
2013-07-20 20:02:07 +0400

    h

--- lua/addons/hush.lua
+++
@@ -1,5 +0,0 @@
-hook.Add("PersonSay", "HushHal", function(pl, body, msg)
-	if pl.Handle == "hal1320" and msg.IsEditable and (body:find("^Twitter: ") or body:find("^Pastebin:")) then
-		msg.Body = "Access denied"
-	end
-end)

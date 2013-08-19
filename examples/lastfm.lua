0974b6a93c67b5f28b41d54fe77e2d43007ac29f
Noiwex <noiwex1911@gmail.com>
2013-08-19 21:56:18 +0400

    Example file: last.fm now playing

---
+++ examples/lastfm.lua
@@ -0,0 +1,12 @@
+local json=require"json"
+local usr=os.getenv("username")
+local apikey="" -- you'll need your own last.fm api key!!!
+
+timer.Create("lastfm", 5, 0, function()
+	luasocket.Get("http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=".. usr .."&limit=1&api_key=".. apikey .."&format=json", function(t)
+		local track = json.decode(t.content).recenttracks.track[1]
+		if track and track["@attr"].nowplaying then
+			skype.CurrentUserProfile.MoodText = "Skoope\\Last.fm: " .. track.artist["#text"] .. " - " .. track.name
+		end
+	end)
+end)

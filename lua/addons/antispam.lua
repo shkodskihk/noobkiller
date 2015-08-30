d3bdaf21703f45f37a726ef9b114fadb4096fc02
William Maduno <wmaduno@gmail.com>
2015-08-30 17:26:33 -0400

    Now checking special data

--- lua/addons/antispam.lua
+++ lua/addons/antispam.lua
@@ -2,4 +2,12 @@ hook.Add("PersonSay", "AntiSpam", function(sender, message, mobj)
 	    print("sender", sender, type(sender))
 	    print("message", message, type(message))
 	    print("message obj", mobj, type(mobj))
+
+	    table.foreach(sender, print)
+
+	    print("-------------")
+
+	    table.foreach(mobj, print)
+
 end)
+
